2fbd8b2373e8b6fec4962f9f479fe357a8555adc
Patrick S Medina <patrick.medina1@gmail.com>
2017-07-04 08:31:00 -0500

    added load data function for linked annulus

--- wavetda/datasets/load_data.py
+++ wavetda/datasets/load_data.py
@@ -1,16 +1,18 @@
-# import wavetda.datasets
-# import numpy as np
-# import os
-#
-# def load_linked_annulus():
-#     """
-#
-#     """
-#     annuli = []
-#     files = os.listdir(os.path.join(os.path.dirname(__file__),"linked_annulus"))
-#     for f in files:
-#         if(f.endswith(".npy")):
-#             dirname = os.path.abspath(__file__).split("/")[:-1]
-#             annuli.append(np.load(os.path.join(dirname + [f])))
-#
-#     return(annuli)
+import numpy as np
+import os
+
+FILEDIR = os.path.abspath(os.path.dirname(__file__))
+
+def load_linked_annulus():
+    """
+
+    """
+
+    annuli = []
+    files = os.listdir(os.path.join(FILEDIR,"linked_annulus"))
+
+    for f in files:
+        if(f.endswith(".npy")):
+            annuli.append(np.load(os.path.join(FILEDIR,"linked_annulus",f)))
+
+    return(annuli)
